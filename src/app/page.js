13f9ac760ce3117de78235fd17aea0087cedc58d
choisukjune <sukjune.choi@gmail.com>
2023-08-28 19:45:37 +0900

    test

--- src/app/page.js
+++ src/app/page.js
@@ -75,8 +75,6 @@ export default function Home() {
           <path stroke-linecap="round" stroke-linejoin="round" d="M13.19 8.688a4.5 4.5 0 011.242 7.244l-4.5 4.5a4.5 4.5 0 01-6.364-6.364l1.757-1.757m13.35-.622l1.757-1.757a4.5 4.5 0 00-6.364-6.364l-4.5 4.5a4.5 4.5 0 001.242 7.244" />
         </svg>
         </Link>
-
-
         </p>
         <p className="mt-1 truncate text-xs leading-5 text-gray-500">{so.names.EN}</p>
       </div>
