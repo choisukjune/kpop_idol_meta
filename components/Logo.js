ef87772081fb2ea7bc605ff81e970700c5d95af4
choisukjune <sukjune.choi@gmail.com>
2023-09-01 10:10:40 +0900

    test

--- components/Logo.js
+++ components/Logo.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Logo = () => {
-  return <div className="logo">CodeWithMarish</div>;
+  return (
+    <svg className="mb-5" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 48 48">
+      <path fill="#039be5" d="M43,38c0,0,0-7.626,0-9.961C43,26.013,41,25,40.429,25C39.014,25,37,27.381,37,29.053V38H43z"></path><path fill="#039be5" d="M5,38c0,0,0-7.626,0-9.961C5,26.013,7,25,7.571,25C8.986,25,11,27.381,11,29.053V38H5z"></path><path fill="#aed581" d="M13,24.54V29H5.63c-0.08-0.3-0.15-0.6-0.21-0.92h0.01C5.17,26.78,5,25.31,5,23.65 C5,21.88,6.54,21,8.43,21C10.31,21,13,23.08,13,24.54z"></path><path fill="#aed581" d="M35,24.54V29h7.37c0.08-0.3,0.15-0.6,0.21-0.92h-0.01c0.26-1.3,0.43-2.77,0.43-4.43 c0-1.77-1.54-2.65-3.43-2.65C37.69,21,35,23.08,35,24.54z"></path><rect width="4" height="6" x="5" y="38" fill="#ffccbc"></rect><rect width="4" height="6" x="39" y="38" fill="#ffccbc"></rect><path fill="#7cb342" d="M13,29v2.62c0,0.96,0,5.31,0,5.31s-1.71,0.44-3.43-0.89C8.96,35.57,6.74,33.36,5.63,29H13z"></path><path fill="#7cb342" d="M35,29v2.62c0,0.96,0,5.31,0,5.31s1.71,0.44,3.43-0.89c0.61-0.47,2.83-2.68,3.94-7.04H35z"></path><g><path fill="#e7ebed" d="M39,9.8v28.201H9V9.8C9,7.112,10.65,5,12.75,5c1.8,0,3.257,1.546,3.654,3.667 c4.846-0.792,10.346-0.792,15.193,0.01C31.982,6.546,33.45,5,35.25,5C37.35,5,39,7.112,39,9.8z"></path><ellipse cx="24" cy="18.5" fill="#ffccbc" rx="12" ry="7.5"></ellipse></g><g><path fill="#29b6f6" d="M39,26v18H9V26l1.54,0.604c3.45,1.335,8.36,2.109,13.46,2.109s10.01-0.774,13.46-2.109 L39,26z"></path></g><g><path fill="none" stroke="#212121" strokeLinecap="round" strokeLinejoin="round" strokeMiterlimit="10" strokeWidth="2" d="M20,18c2.751,1.27,5.426,1.378,8,0"></path></g><g><circle cx="31" cy="16" r="1" fill="#212121"></circle><circle cx="17" cy="16" r="1" fill="#212121"></circle></g>
+    </svg>
+  );
 };
 
 export default Logo;
